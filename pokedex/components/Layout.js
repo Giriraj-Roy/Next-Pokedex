cf9742642791eee02b335dc21bfb4f475ba27d79
Giriraj Roy <girirajroy2001@gmail.com>
2022-03-25 18:01:42 +0530

    Layout

--- pokedex/components/Layout.js
+++ pokedex/components/Layout.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import Head from 'next/Head'
+import React, { Children } from 'react'
 
-export default function Layout() {
+export default function Layout({title}) {
   return (
-    <div>Layout</div>
+    <div className='bg-gray-300'>
+        <Head>
+            <title>{title}</title>
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <main className='container mx-auto max-w-xl pt-8 min-h-screen'>
+            {/* {children} */}
+            {/* Kuch Bhi */}
+        </main>
+    </div>
   )
 }
